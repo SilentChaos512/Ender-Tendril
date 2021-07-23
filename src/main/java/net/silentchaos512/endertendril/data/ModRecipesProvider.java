370c9298e47eb7584b7b41b14848aae759c436aa
SilentChaos512 <sworkman256@gmail.com>
2021-07-23 18:55:52 -0500

    Add ender eye recipe

--- src/main/java/net/silentchaos512/endertendril/data/ModRecipesProvider.java
+++ src/main/java/net/silentchaos512/endertendril/data/ModRecipesProvider.java
@@ -3,6 +3,10 @@ package net.silentchaos512.endertendril.data;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapelessRecipeBuilder;
+import net.minecraft.item.Items;
+import net.silentchaos512.endertendril.EnderTendrilMod;
+import net.silentchaos512.endertendril.setup.ModItems;
 
 import java.util.function.Consumer;
 
@@ -12,7 +16,11 @@ public class ModRecipesProvider extends RecipeProvider {
     }
 
     @Override
-    protected void registerRecipes(Consumer<IFinishedRecipe> p_200404_0_) {
-        // TODO
+    protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        ShapelessRecipeBuilder.shapelessRecipe(Items.ENDER_EYE)
+                .addIngredient(ModItems.TENDRIL_PEARL.get(), 2)
+                .addIngredient(Items.BLAZE_POWDER)
+                .addCriterion("has_item", hasItem(ModItems.TENDRIL_PEARL.get()))
+                .build(consumer, EnderTendrilMod.getId("ender_eye"));
     }
 }
