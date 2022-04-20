1ed5ad271f5d6e8106a3de3fcc1cfe56e7f3e103
SilentChaos512 <sworkman256@gmail.com>
2022-04-19 20:45:46 -0500

    Update to Minecraft 1.18.2

--- src/main/java/net/silentchaos512/endertendril/setup/ModTags.java
+++ src/main/java/net/silentchaos512/endertendril/setup/ModTags.java
@@ -1,12 +1,12 @@
 package net.silentchaos512.endertendril.setup;
 
-import net.minecraft.world.level.block.Block;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
 import net.silentchaos512.endertendril.EnderTendrilMod;
 
 public class ModTags {
     public static class Blocks {
-        public static final Tag.Named<Block> ENDER_TENDRILS = BlockTags.bind(EnderTendrilMod.getId("ender_tendrils").toString());
+        public static final TagKey<Block> ENDER_TENDRILS = BlockTags.create(EnderTendrilMod.getId("ender_tendrils"));
     }
 }
